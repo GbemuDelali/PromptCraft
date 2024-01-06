b2d9b6a4dd53494e481de8489bce8e4fa30b6229
Delali Gbemu <146733488+GbemuDelali@users.noreply.github.com>
2024-01-06 12:53:54 +0000

    Add files via upload

---
+++ documentation/documentation.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const coll = document.querySelectorAll('.collapsible');
+
+    coll.forEach(function (item) {
+      item.addEventListener('click', function () {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + 'px';
+        }
+      });
+    });
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
