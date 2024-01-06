546e0be99353952caf689943f1ab6fcf278b7f3f
Delali Gbemu <146733488+GbemuDelali@users.noreply.github.com>
2024-01-06 13:14:43 +0000

    Delete example directory

--- example/example.js
+++
@@ -1,5 +0,0 @@
-function executeExample(exampleNumber) {
-    // Logic to execute the example using JavaScript or any backend integration
-    // Display the output in the respective output div
-    document.getElementById(`output${exampleNumber}`).innerHTML = "Output will be displayed here.";
-  }
